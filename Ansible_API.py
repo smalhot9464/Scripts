7e9c73a5f144bfed85d2f9c26b6a8f3bccffc3cd
Shivam Malhotra <36483172+smalhot9464@users.noreply.github.com>
2022-03-29 14:26:46 +0530

    Update Ansible_API.py

--- Ansible_API.py
+++ Ansible_API.py
@@ -10,7 +10,6 @@ class clsinfo(restful.Resource):
         self.data = request.get_json()
         self.contactno = self.data['contactno']
         self.myid= self.data['myid']
-        
 
         try:
             ansible_cmd = '/etc/ansible/myansible/ansible-playbook -i %s, /home/shivam/myplaybook.yml -e myid=%s -e contactno=%s ' % (self.contactno, self.contactno, self.myid)
